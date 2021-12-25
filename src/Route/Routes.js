ec15998a5d86dab3494385a086b43f8cf6a0a90e
Mohammed Saif <saifmohammed888@gmail.com>
2021-12-25 23:01:12 +0530

    Netlify changes

--- src/Route/Routes.js
+++ src/Route/Routes.js
@@ -6,7 +6,7 @@ import PublicRoute from "Route/PublicRoute";
 // import { Route } from 'react-router-dom/cjs/react-router-dom.min';
 
 const Landing = lazy(() => import("component/Landing/Landing"))
-// const About = lazy(() => import("component/About/About"))
+const About = lazy(() => import("component/About/About"))
 // const Package = lazy(() => import("component/Package/Package"))
 // const Join = lazy(() => import("component/Join/Join"))
 // const Contact = lazy(() => import("component/Contact/Contact"))
@@ -18,27 +18,11 @@ const Landing = lazy(() => import("component/Landing/Landing"))
 
 export default function Routes() {
 
-    const loginState = useSelector(state => state.authReducer);
-
     return (
         <div>
-            <PublicRoute exact path="/" component={Landing}  />
-            <Route path="/" component={ Landing} />
-            {/* <PublicRoute exact path="/about" component={About}  loggedIn={loginState.isLoggedIn} restricted="false" />
-            <PublicRoute exact path="/package" component={Package}  loggedIn={loginState.isLoggedIn} restricted="false"/>
-            <PublicRoute exact path="/join" component={Join}  loggedIn={loginState.isLoggedIn} restricted="false"/>
-            <PublicRoute exact path="/contact" component={Contact}  loggedIn={loginState.isLoggedIn} restricted="false"/>
-            <PublicRoute exact path="/signIn" component={SignIn}  loggedIn={loginState.isLoggedIn} restricted="false" /> */}
-            {/* <PrivateRoute exact path="/admin/login" component={AdminLogin} loggedIn={loginState}/>
-            <PrivateRoute  path="/admin" component={Admin} loggedIn={loginState}/>
-            <PrivateRoute  path="/admin/login" component={AdminLogin} loggedIn={loginState.isLoggedIn}/>
-            <PrivateRoute path="/admin" component={Admin} loggedIn={loginState.isLoggedIn} />
-            <PrivateRoute  path={`/quizContent/:quizId?`} component={QuizContent} loggedIn={loginState.isLoggedIn} /> */} 
-
-            {/* <Route path="/admin" component={Admin} />
-            <Route path="/interviewer" component={Interviewer} />
-            <Route path="/student" component={Student} />
-            <Route path={`/quizContent/:quizId?`} component={QuizContent} /> */}
+            <PublicRoute exact path="/" component={Landing} />
+            <PublicRoute exact path="/about" component={About}  />
+           
         </div>
     )
 }
