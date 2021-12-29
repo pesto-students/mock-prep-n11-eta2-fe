4482bfea1f80e2b17b94d7eb397c3d36c3b50dc8
Mohammed Saif <saifmohammed888@gmail.com>
2021-12-30 00:00:58 +0530

    Deploy netlify

--- src/index.js
+++ src/index.js
@@ -12,10 +12,6 @@ import { Integrations } from "@sentry/tracing";
 Sentry.init({
   dsn: sentrydsn,
   integrations: [new Integrations.BrowserTracing()],
-
-  // Set tracesSampleRate to 1.0 to capture 100%
-  // of transactions for performance monitoring.
-  // We recommend adjusting this value in production
   tracesSampleRate: 1.0,
 });
 
