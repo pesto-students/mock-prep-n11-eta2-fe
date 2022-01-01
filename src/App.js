32f72949d04661cac6a4c9beaabc8d36dd91cc67
Mohammed Saif <saifmohammed888@gmail.com>
2022-01-01 22:12:19 +0530

    Final

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Switch } from "react-router-dom";
 import { fallback } from "constant/navList";
 import Routes from "Route/Routes";
 
-import "bootstrap/dist/css/bootstrap.min.css";
+// import "bootstrap/dist/css/bootstrap.min.css";
 import "antd/dist/antd.min.css";
 import "./App.css";
 
